8e01b5edfa41e814914ba3095e472355a016e61a
Shiva Mahitha Maddi <59783725+Mahitha-Maddi@users.noreply.github.com>
2021-03-30 18:01:05 -0400

    Update SignIn.js

--- fe/src/pages/SignIn/SignIn.js
+++ fe/src/pages/SignIn/SignIn.js
@@ -124,9 +124,9 @@ const SignIn = () => {
     console.log(paramdict);
 
     // verify user/pwd, get encoded userid as access and refresh tokens in return
-    fetch("http://localhost:5000/login", config)
+    //fetch("http://localhost:5000/login", config)
     //fetch(`${process.env.BE_NETWORK}:${process.env.BE_PORT}/login`, config)
-    //fetch(`${process.env.REACT_APP_API_SERVICE_URL}/login`, config)
+    fetch(`${process.env.REACT_APP_API_SERVICE_URL}/login`, config)
       .then(response => response.json())
       .then(data => {
 
