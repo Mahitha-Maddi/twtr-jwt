04db70d2fd3fbb86f3e8b5f56d88d2145052c7af
Mahitha-Maddi <maddi.s@husky.neu.edu>
2021-03-29 23:36:23 -0400

    build folder

---
+++ fe/build/precache-manifest.a3a4d3a4df709898d6d529050aa38f4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0311e9b02b198f090d8553335c5589d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "613f3d81e9a1395c2099",
+    "url": "/static/css/main.643b110a.chunk.css"
+  },
+  {
+    "revision": "a5c4c9f79d1ff53a96f7",
+    "url": "/static/js/2.85d2fc21.chunk.js"
+  },
+  {
+    "revision": "9a5d22d7b0e21afebd28f4768943f0bf",
+    "url": "/static/js/2.85d2fc21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "613f3d81e9a1395c2099",
+    "url": "/static/js/main.b9607773.chunk.js"
+  },
+  {
+    "revision": "07ef167960929c3ab583",
+    "url": "/static/js/runtime-main.60388dba.js"
+  }
+]);
